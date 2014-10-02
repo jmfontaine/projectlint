e0857bff3a2580cfa417b3653ba3347da97b336f
Jean-Marc Fontaine <jm@jmfontaine.net>
2014-10-02 22:20:52 +0200

    Fix a coding style violation

--- src/ExpressionLanguage/ExpressionLanguage.php
+++ src/ExpressionLanguage/ExpressionLanguage.php
@@ -10,13 +10,13 @@ class ExpressionLanguage extends BaseExpressionLanguage
         parent::registerFunctions();
 
         $this->register('lowercase', function ($str) {
-                return sprintf('(is_string(%1$s) ? strtolower(%1$s) : %1$s)', $str);
-            }, function ($arguments, $str) {
-                if (!is_string($str)) {
-                    return $str;
-                }
+            return sprintf('(is_string(%1$s) ? strtolower(%1$s) : %1$s)', $str);
+        }, function ($arguments, $str) {
+            if (!is_string($str)) {
+                return $str;
+            }
 
-                return strtolower($str);
-            });
+            return strtolower($str);
+        });
     }
 }
